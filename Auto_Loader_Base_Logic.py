3caef1a86b037323e2e8bc9f52c51103bbc5633a
erict7554 <eric@re3d.org>
2025-07-02 11:49:02 -0500

    Testing Pin High

--- Auto_Loader_Base_Logic.py
+++ Auto_Loader_Base_Logic.py
@@ -1,1 +1,19 @@
-print('Goodbye')
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Set up GPIO pin 17 as an output
+GPIO.setup(17, GPIO.OUT)
+
+try:
+    while True:
+        GPIO.output(17, GPIO.HIGH)  # Turn on
+        time.sleep(5)
+        GPIO.output(17, GPIO.LOW)   # Turn off
+        time.sleep(5)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
