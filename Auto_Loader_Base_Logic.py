db0983af8404814797771bbf2d292df2a9d9c47e
erict7554 <eric@re3d.org>
2025-07-02 13:16:44 -0500

    Tried Fix

--- Auto_Loader_Base_Logic.py
+++ Auto_Loader_Base_Logic.py
@@ -1,19 +1,14 @@
 import RPi.GPIO as GPIO
 import time
-print('Changes made to Auto_Loader_Base_Logic.py')
+print('Changes made to Auto_Loader_Base_Logic.py 2')
 # Set the GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
 
 # Set up GPIO pin 17 as an output
 GPIO.setup(17, GPIO.OUT)
 
-try:
-    while True:
-        GPIO.output(17, GPIO.HIGH)  # Turn on
-        time.sleep(5)
-        GPIO.output(17, GPIO.LOW)   # Turn off
-        time.sleep(5)
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
+while True:
+    GPIO.output(17, GPIO.HIGH)  # Turn on
+    time.sleep(5)
+    GPIO.output(17, GPIO.LOW)   # Turn off
+    time.sleep(5)
