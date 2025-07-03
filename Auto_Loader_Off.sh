bcdb2ab4f1d7a65f788f8fb600f90ed55ebca332
Mitchell-Mashburn <mitchell@re3d.org>
2025-07-03 15:36:51 -0500

    Add script to kill auto loader process

---
+++ Auto_Loader_Off.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+PID=$(pgrep -f Auto_Loader_Base_Logic.py)
+
+if [ -z "$PID" ]; then
+    echo "No running instance of duty_cycle.py found."
+else
+    kill "$PID"
+    echo "Stopped duty_cycle.py with PID $PID."
+fi
