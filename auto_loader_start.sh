c760115a7575ec2d4b59433b92510da7958748ad
erict7554 <eric@re3d.org>
2025-07-09 10:27:36 -0500

    changed back

--- auto_loader_start.sh
+++ auto_loader_start.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# $1 will contain the single string of space-separated parameters
-# Use 'read -r -a' to split this string into an array called params_array
-read -r -a params_array <<< "$1"
+# Parameters passed from Klipper macro
+ACTIVATION_DELAY=$1
+EXTRA_FILL_TIME=$2
+TIMEOUT=$3
 
-# Assign individual parameters from the array
-ACTIVATION_DELAY=${params_array[0]}
-EXTRA_FILL_TIME=${params_array[1]}
-TIMEOUT=${params_array[2]}
-
-# print the received parameters
-echo "Received parameters: ACTIVATION_DELAY=${ACTIVATION_DELAY}, EXTRA_FILL_TIME=${EXTRA_FILL_TIME}, TIMEOUT=${TIMEOUT}"
-
-# Kill any existing instances of the script
+# Kill any existing instances of the script (optional, but good for reliable restarts)
 pkill -f "Auto_Loader_Base_Logic.py"
 
-# Start your Python program with the parsed parameters
-# Ensure the absolute path to your Python script is correct
+# Start your Python program with the parameters
 sudo python3 /home/pi/Auto_Loader_Base_Logic.py "${ACTIVATION_DELAY}" "${EXTRA_FILL_TIME}" "${TIMEOUT}" &
 
 exit 0
