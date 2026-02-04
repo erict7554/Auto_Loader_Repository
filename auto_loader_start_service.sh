ba8594ffcb7d2ca28c91075d9da79552e5d20058
erict7554 <eric@re3d.org>
2026-02-04 14:40:42 -0600

    Remove auto_loader service helper scripts
    Delete auto_loader_start_service.sh and auto_loader_stop_service.sh. The removed scripts previously started a templated auto_loader@<params> systemd service and stopped running auto_loader@ instances, respectively; they are no longer present in the repository.

--- auto_loader_start_service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Usage: ./auto_loader_start.sh ACTIVATION_DELAY EXTRA_FILL_TIME TIMEOUT
-
-ACTIVATION_DELAY=$1
-EXTRA_FILL_TIME=$2
-TIMEOUT=$3
-
-# Start the systemd templated service with the parameters
-sudo systemctl start "auto_loader@${ACTIVATION_DELAY}:${EXTRA_FILL_TIME}:${TIMEOUT}"
